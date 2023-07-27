4cabff3ca9d2e45c9c8770cd898a2d135d11dfbf
Emma <ep.dance@gmail.com>
2023-07-27 15:06:25 -0400

    first commit

---
+++ gentile/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SurveyComponent from './SurveyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SurveyComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ gentile/src/json.js
@@ -0,0 +1,342 @@
+export const json = {
+    "title": "Gentile Retina",
+    "logo": "https://api.surveyjs.io/private/Surveys/files?name=0e517195-c423-4a92-8f68-d395493b3722",
+    "logoPosition": "right",
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question1",
+        "title": "Dr. Gentile smiled and made me feel welcome.",
+        "autoGenerate": false,
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ]
+       },
+       {
+        "type": "rating",
+        "name": "question2",
+        "title": "Dr. Gentile treated and addressed me as a person, not a number or medical diagnosis."
+       },
+       {
+        "type": "rating",
+        "name": "question3",
+        "title": "Dr. Gentile was respectful of my time."
+       },
+       {
+        "type": "rating",
+        "name": "question5",
+        "title": "Dr. Gentile understood my concerns and cared about me."
+       },
+       {
+        "type": "rating",
+        "name": "question6",
+        "title": "Dr. Gentile was friendly."
+       },
+       {
+        "type": "rating",
+        "name": "question7",
+        "title": "Dr. Gentile addressed all my concerns and answered all my questions."
+       },
+       {
+        "type": "rating",
+        "name": "question8",
+        "title": "How satisfied were you with Dr. Gentile today?",
+        "description": "1 = unsatisfied, 5 = very satisfied",
+        "rateType": "smileys"
+       },
+       {
+        "type": "rating",
+        "name": "question9",
+        "title": "On a scale of 1-10, how likely is it that you would recommend Dr. Gentile to someone else?",
+        "rateCount": 10,
+        "rateMax": 10
+       },
+       {
+        "type": "comment",
+        "name": "question10",
+        "title": "What is the primary reason for your score in the two previous questions?"
+       },
+       {
+        "type": "comment",
+        "name": "question11",
+        "title": "Additional comments or feedback for Dr. Gentile:"
+       }
+      ],
+      "title": "Dr. Ronald Gentile",
+      "description": "Please rate the following statements from 1 to 5 for Dr. Gentile: 1 = Strongly Disagree, 2 = Disagree , 3 = Neutral , 4 = Agree, 5 = Strongly Agree"
+     },
+     {
+      "name": "page2",
+      "elements": [
+       {
+        "type": "imagepicker",
+        "name": "question4",
+        "title": "Which technician helped you in the exam room?",
+        "choices": [
+         {
+          "value": "Image 1",
+          "text": "Felo",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=94326453-1724-48c7-a289-76086a2f82fb"
+         },
+         {
+          "value": "Image 2",
+          "text": "Connie",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=cef82811-2c26-483e-ac9b-e447f557a658"
+         },
+         {
+          "value": "Image 3",
+          "text": "Diana",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=ab402dc3-7ef9-4439-8c44-d41868936ea0"
+         },
+         {
+          "value": "Image 4",
+          "text": "Himani",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=76bc7da8-8c77-4cbd-9deb-1fbd7a6ab575"
+         },
+         {
+          "value": "Image 5",
+          "text": "Jake",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=624c3faa-233b-49eb-8802-aada179ade34"
+         },
+         {
+          "value": "Image 6",
+          "text": "Jennifer",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=649d8711-0e8e-4b0c-a0ce-82a7cda04f21"
+         },
+         {
+          "value": "Image 7",
+          "text": "Julia",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=7b7b7bb5-d17e-45fa-8e52-02a24ea57684"
+         },
+         {
+          "value": "Image 8",
+          "text": "Kazi",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=1f45a87a-fcde-4089-a40d-41bf53502b8e"
+         },
+         {
+          "value": "Image 9",
+          "text": "Sophie",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=79f863a7-0400-4165-a3a7-916374ccc821"
+         },
+         {
+          "value": "Image 10",
+          "text": "Nervani",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=0a616bf1-bc15-4303-9c0b-4ea977cd8114"
+         },
+         {
+          "value": "Image 11",
+          "text": "Chenxi",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=62a4e4ca-2aaa-4aa7-9862-b4124a71a33a"
+         }
+        ],
+        "imageFit": "cover",
+        "imageWidth": 100,
+        "showLabel": true
+       }
+      ],
+      "title": "Technician Evaluation"
+     },
+     {
+      "name": "page3",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question12",
+        "title": "The technician smiled and made me feel welcome."
+       },
+       {
+        "type": "rating",
+        "name": "question13",
+        "title": "The technician treated and addressed me as a person, not a number or medical diagnosis."
+       },
+       {
+        "type": "rating",
+        "name": "question14",
+        "title": "The technician was respectful of my time."
+       },
+       {
+        "type": "rating",
+        "name": "question15",
+        "title": "The technician respected and was sensitive to my cultural background. If not applicable to your visit, please select \"N/A.\"",
+        "autoGenerate": false,
+        "rateCount": 6,
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5,
+         {
+          "value": 6,
+          "text": "N/A"
+         }
+        ],
+        "rateMax": 6
+       },
+       {
+        "type": "rating",
+        "name": "question16",
+        "title": "The technician understood my concerns and cared for me."
+       },
+       {
+        "type": "rating",
+        "name": "question17",
+        "title": "The technician was friendly."
+       },
+       {
+        "type": "text",
+        "name": "question18",
+        "title": "The technician addressed all my concerns and answered all my questions."
+       },
+       {
+        "type": "rating",
+        "name": "question19",
+        "title": "How satisfied were you with the technician today?",
+        "rateType": "smileys"
+       },
+       {
+        "type": "rating",
+        "name": "question20",
+        "title": "On a scale of 1-10, how likely is it that you would recommend the technician to someone else?",
+        "description": "1 = very unikely, 10 = very likely ",
+        "rateCount": 10,
+        "rateMax": 10
+       },
+       {
+        "type": "comment",
+        "name": "question21",
+        "title": "What is the primary reason for your score in the two previous questions?"
+       },
+       {
+        "type": "comment",
+        "name": "question22",
+        "title": "Additional comments or feedback for the technician:"
+       }
+      ],
+      "title": "Technician Evaluation",
+      "description": "Please rate the following statements from 1 to 5 (or in some instances \"N/A\")"
+     },
+     {
+      "name": "page4",
+      "elements": [
+       {
+        "type": "imagepicker",
+        "name": "question23",
+        "title": "Which member of the front desk helped you today?",
+        "choices": [
+         {
+          "value": "Image 1",
+          "text": "Evangeline",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=a3153843-2da4-4802-a8ff-0aed7c460f5f"
+         },
+         {
+          "value": "Image 2",
+          "text": "Giselle",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=20d111a0-5630-4d61-8149-4bf13626fc9d"
+         },
+         {
+          "value": "Image 3",
+          "text": "Titus",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=a49abe20-cfa1-4993-8fbb-349814469a29"
+         },
+         {
+          "value": "Image 4",
+          "text": "Alice",
+          "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=471a9c8f-dbd2-4ced-9609-41060c499a9b"
+         }
+        ],
+        "showLabel": true
+       }
+      ],
+      "title": "Front Desk Evaluation"
+     },
+     {
+      "name": "page5",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question24",
+        "title": "The receptionist smiled and made me feel welcome."
+       },
+       {
+        "type": "rating",
+        "name": "question25",
+        "title": "The receptionist treated and addressed me as a person, not a number or medical diagnosis."
+       },
+       {
+        "type": "rating",
+        "name": "question26",
+        "title": "The receptionist was respectful of my time."
+       },
+       {
+        "type": "rating",
+        "name": "question27",
+        "title": "The receptionist respected and was sensitive to my cultural background. If not applicable to your visit, please select \"N/A.\"",
+        "autoGenerate": false,
+        "rateCount": 6,
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5,
+         {
+          "value": 6,
+          "text": "N/A"
+         }
+        ],
+        "rateMax": 6
+       },
+       {
+        "type": "rating",
+        "name": "question28",
+        "title": "The receptionist understood my concerns and cared about me."
+       },
+       {
+        "type": "rating",
+        "name": "question29",
+        "title": "The receptionist was friendly."
+       },
+       {
+        "type": "rating",
+        "name": "question30",
+        "title": "The receptionist 7. Alice addressed all my concerns and answered all my questions."
+       },
+       {
+        "type": "rating",
+        "name": "question31",
+        "title": "How satisfied were you with Alice today?",
+        "rateType": "smileys"
+       },
+       {
+        "type": "rating",
+        "name": "question32",
+        "title": "On a scale of 1-10, how likely is it that you would recommend Alice to someone else?",
+        "description": "1 = very unlikely, 10 = very likely",
+        "rateCount": 10,
+        "rateMax": 10
+       },
+       {
+        "type": "comment",
+        "name": "question33",
+        "title": "What is the primary reason for your score in the two previous questions?"
+       },
+       {
+        "type": "comment",
+        "name": "question34",
+        "title": "11. Additional comments or feedback for Alice:"
+       }
+      ],
+      "title": "Front Desk Evaluation",
+      "description": "Please rate the following statements from 1 to 5 (or in some instances \"N/A\") "
+     }
+    ],
+    "cookieName": "cookies"
+   }
